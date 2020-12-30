7cebf90fdf856b3b6324c0f0645e24d592a968e1
Rich Dammkoehler <rpd@noradltd.com>
2020-12-29 20:45:42 -0600

    heavy refactors in Main for readability

--- src/main/java/com/noradltd/wumpus/Main.java
+++ src/main/java/com/noradltd/wumpus/Main.java
@@ -1,23 +1,30 @@
 package com.noradltd.wumpus;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.Collections.unmodifiableMap;
+
 public class Main {
+
+    public static final Pattern USER_COMMAND = Pattern.compile("\\s*(\\S+)\\s*?(\\d*)?\\s*");
+    private Game game;
+    private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
     interface Command {
         void execute(Game game, String arg);
     }
 
-    private final Map<String, Command> COMMANDS = new HashMap() {
+    private final Map<String, Command> COMMANDS = unmodifiableMap(new HashMap<>() {
         @Override
-        public Object get(Object key) {
-            return super.getOrDefault(key, (Command) (game, arg) -> Logger.info("What?"));
+        public Command get(Object key) {
+            return super.getOrDefault(key, (game, arg) -> Logger.info("What?"));
         }
 
         private void addCommands(Command command, String... inputStrings) {
@@ -32,69 +39,81 @@ public class Main {
             addCommands((game, arg) -> showHelp(), "?", "h", "help");
             addCommands((game, arg) -> game.isPlaying(), "l", "look");
         }
-    };
 
-    void play(InputStream inputStream, String... options) {
+        private Integer exitNumber(String arg) {
+            return Integer.parseInt(arg) - 1;
+        }
+    });
+
+    private void play(String... options) {
         try {
             Logger.info("Welcome to Hunt The Wumpus!");
-            final Pattern pattern = Pattern.compile("\\s*(\\S+)\\s*?(\\d*)?\\s*");
-            Game game = new Game(options);
+            game = new Game(options);
             while (game.isPlaying()) {
-                Command command = COMMANDS.get("what");
-                String arg = null;
-                Logger.info(game.toString());
-                Logger.info("move/shoot?");
-                try {
-                    Matcher matcher = pattern.matcher(nextCommand(inputStream));
-                    if (matcher.matches()) {
-                        String action = matcher.group(1);
-                        arg = matcher.group(2);
-                        command = COMMANDS.get(action);
-                    }
-                } catch (IOException ex) {
-                    Logger.info("IO error");
-                } finally {
-                    command.execute(game, arg);
-                }
+                executeUserCommands();
             }
         } catch (Throwable thrown) {
+            Logger.error("something went terribly wrong.");
             thrown.printStackTrace(System.err);
-            Logger.info("something went terribly wrong.");
         } finally {
             // TODO display score!
+            Logger.info("Score: Hunter ? Wumpus ?");
         }
     }
 
-    private String nextCommand(InputStream inputStream) throws IOException {
-        StringBuilder sb = new StringBuilder();
-        int data = inputStream.read();
-        while (data != 10) {
-            sb.append((char) data);
-            data = inputStream.read();
+    class CommandArg {
+        Command command;
+        String arg;
+
+        CommandArg(Command command, String arg) {
+            this.command = command;
+            this.arg = arg;
         }
-        return sb.toString();
+    }
+
+    private void executeUserCommands() {
+        promptUser();
+        CommandArg commandArg = getUserCommand();
+        commandArg.command.execute(game, commandArg.arg);
+    }
+
+    private CommandArg getUserCommand() {
+        Command command = COMMANDS.get("what");
+        String arg = null;
+        Matcher matcher = USER_COMMAND.matcher(nextCommand());
+        if (matcher.matches()) {
+            String action = matcher.group(1);
+            arg = matcher.group(2);
+            command = COMMANDS.get(action);
+        }
+        return new CommandArg(command, arg);
+    }
+
+    private void promptUser() {
+        Logger.info(game.toString() + "\n" + "move/shoot?");
+    }
+
+    private String nextCommand() {
+        try {
+            return input.readLine();
+        } catch (IOException e) {
+            return "help";
+        }
     }
 
     private void showHelp() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Instructions:\n");
-        sb.append("(m|move) #\t\t\tMove through tunnel #\n");
-        sb.append("(s|shoot) #\t\t\tShoot through tunnel #\n");
-        sb.append("(l|look)\t\t\tLook around\n");
-        sb.append("(i|inv|inventory)\tShow inventory\n");
-        sb.append("(?|h|help)\t\t\tShow help\n");
-        sb.append("(q|x|quit|exit)\t\tQuit the game\n");
-        // TODO command line options
-        Logger.info(sb.toString());
+        // TODO what about command line args?
+        Logger.info("Instructions:\n" +
+                "(m|move) #\t\t\tMove through tunnel #\n" +
+                "(s|shoot) #\t\t\tShoot through tunnel #\n" +
+                "(l|look)\t\t\tLook around\n" +
+                "(i|inv|inventory)\tShow inventory\n" +
+                "(?|h|help)\t\t\tShow help\n" +
+                "(q|x|quit|exit)\t\tQuit the game\n");
     }
-
-    private int exitNumber(String arg) {
-        return Integer.parseInt(arg) - 1;
-    }
-
 
     public static void main(String[] args) {
-        new Main().play(System.in, args);
+        new Main().play(args);
         // TODO play again?
         Logger.info("Goodbye");
     }
