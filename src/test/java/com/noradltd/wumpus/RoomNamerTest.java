dcd237716612b7780132140e98f1bbaff92db34e
rdammkoehler <rpd@noradltd.com>
2022-08-24 14:54:57 -0500

    test cleanup; dedupe, const, etc.

--- src/test/java/com/noradltd/wumpus/RoomNamerTest.java
+++ src/test/java/com/noradltd/wumpus/RoomNamerTest.java
@@ -10,6 +10,8 @@ import static org.hamcrest.Matchers.*;
 // todo,need factory wrapper on the impl; these test are for the interface level
 public class RoomNamerTest {
 
+    private static final String[] NAMES = {"Test Room 1", "Test Room 2"};
+
     @Test
     public void aRoomNamerGeneratesARoomName() {
         String roomName = new ListBasedRoomNameGenerator().nextName();
@@ -19,46 +21,38 @@ public class RoomNamerTest {
 
     @Test
     public void aRoomNamerAcceptsAListOfPossibleNames() {
-        String[] possibleNames = {"Test Room 1", "Test Room 2"};
+        String roomName = new ListBasedRoomNameGenerator().using(NAMES).nextName();
 
-        String roomName = new ListBasedRoomNameGenerator().using(possibleNames).nextName();
-
-        assertThat(roomName, in(Arrays.asList(possibleNames)));
+        assertThat(roomName, in(Arrays.asList(NAMES)));
     }
 
     @Test
     public void aRoomNamerReturnsAllTheRoomNames() {
-        String[] possibleNames = {"Test Room 1", "Test Room 2"};
-        RoomNameGenerator nameGenerator = new ListBasedRoomNameGenerator().using(possibleNames);
+        RoomNameGenerator nameGenerator = new ListBasedRoomNameGenerator().using(NAMES);
 
         String[] roomNames = {
                 nameGenerator.nextName(),
                 nameGenerator.nextName()
         };
 
-        assertThat(Arrays.asList(roomNames), containsInAnyOrder("Test Room 1", "Test Room 2"));
+        assertThat(Arrays.asList(roomNames), containsInAnyOrder(NAMES[0], NAMES[1]));
     }
 
     @Test
     public void aRoomNamerReturnsRoomNamesInOrderWhenAsked() {
-        String[] possibleNames = {"Test Room 1", "Test Room 2"};
-        RoomNameGenerator nameGenerator = new ListBasedRoomNameGenerator().sequentially().using(possibleNames);
+        RoomNameGenerator nameGenerator = new ListBasedRoomNameGenerator().sequentially().using(NAMES);
 
         String[] roomNames = {
                 nameGenerator.nextName(),
                 nameGenerator.nextName()
         };
 
-        assertThat(roomNames, equalTo(possibleNames));
+        assertThat(roomNames, equalTo(NAMES));
     }
 
     @Test
     public void aRoomNamerReturnsRoomNamesRandomlyWhenAsked() {
-        String[] possibleNames = new String[26];
-        char varVar = 'a';
-        for (int idx = 0; idx < possibleNames.length; idx++) {
-            possibleNames[idx] = "Test Room " + varVar++;
-        }
+        String[] possibleNames = createPossibleRoomNamesArray();
         RoomNameGenerator nameGenerator = new ListBasedRoomNameGenerator().randomly().using(possibleNames);
 
         String[] roomNames = new String[possibleNames.length];
@@ -72,16 +66,21 @@ public class RoomNamerTest {
 
     @Test
     public void aRoomNamerDoesNotChangeTheOrderOfTheGivenPossibleNamesWhenRandomized() {
-        String[] possibleNames = new String[26];
+        String[] possibleNames = createPossibleRoomNamesArray();
         String[] originalPossibleNames = new String[possibleNames.length];
-        char varVar = 'a';
-        for (int idx = 0; idx < possibleNames.length; idx++) {
-            possibleNames[idx] = "Test Room " + varVar++;
-        }
         System.arraycopy(possibleNames, 0, originalPossibleNames, 0, possibleNames.length);
 
         new ListBasedRoomNameGenerator().randomly().using(possibleNames);
 
         assertThat(originalPossibleNames, equalTo(possibleNames));
+    }
+
+    private String[] createPossibleRoomNamesArray() {
+        String[] possibleNames = new String[26];
+        char roomNameSuffix = 'a';
+        for (int idx = 0; idx < possibleNames.length; idx++) {
+            possibleNames[idx] = "Test Room " + roomNameSuffix++;
+        }
+        return possibleNames;
     }
 }
